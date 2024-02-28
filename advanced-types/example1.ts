fc3fe0a0fa6a50912959bed867456d6bf1ec906e
Alexander Sosov <luisodiz@yandex.ru>
2024-02-28 12:40:15 +0300

    ДЗ 1

---
+++ advanced-types/example1.ts
@@ -0,0 +1,68 @@
+/*
+* // Запрос в виде платежа
+{
+	"sum": 10000,
+	"from": 2,
+	"to": 4
+}
+// Ответ
+{
+	"status": "success",
+	"data": {
+		"databaseId": 567,
+		"sum": 10000,
+		"from": 2,
+		"to": 4
+	}
+},
+{
+	"status": "failed",
+	"data": {
+		"errorMessage": "Недостаточно средств",
+		"errorCode": 4
+	}
+}
+*
+* */
+
+// Необходимо типизировать запрос и ответ
+interface IPayment {
+  sum: number,
+  from: number,
+  to: number
+}
+
+interface IPaymentRequest extends IPayment {
+}
+
+enum PaymentStatus {
+  Success = 'success',
+  Failed = 'failed'
+}
+
+interface IPaymentSuccessData extends IPayment {
+  databaseId: number
+}
+
+interface IPaymentFailedData {
+  errorMessage: string,
+  errorCode: number
+}
+
+interface IPaymentResponseSuccess {
+  status: PaymentStatus.Success,
+  data: IPaymentSuccessData
+}
+
+interface IPaymentResponseFailed {
+  status: PaymentStatus.Failed,
+  data: IPaymentFailedData
+}
+
+// Использование
+// function get(): IPaymentResponseSuccess | IPaymentResponseFailed {
+//   return {
+//     status: PaymentStatus.Failed,
+//     data: {}
+//   }
+// }
