2d61cba42844a8ed58c9c7ec7e697976d9525206
Alexander Sosov <luisodiz@yandex.ru>
2024-03-07 20:43:23 +0300

    type guard

---
+++ advanced-types/12.ts
@@ -0,0 +1,47 @@
+// Type Guard
+
+interface User {
+  name: string,
+  login: string,
+  email: string
+}
+
+const user: User = {
+  name: 'Вася',
+  email: 'vasya@yandex.ru',
+  login: 'vasiliy'
+}
+
+interface Admin {
+  name: string,
+  role: number
+}
+
+function logId(id: string | number) {
+  if (isString(id)) {
+    console.log(id)
+  } else {
+    console.log(id)
+  }
+}
+
+function isString(x: string | number): x is string {
+  return typeof x === 'string'
+}
+
+function setRoleZero(user: User | Admin) {
+  if (isAdminAlternative(user)) {
+    user.role = 0
+  } else {
+    throw new Error('Пользователь не админ')
+  }
+}
+
+// Сделаем type guard на проверку, что пользователь является админом
+function isAdmin(user: User | Admin): user is Admin {
+  return 'role' in user
+}
+
+function isAdminAlternative(user: User | Admin): user is Admin {
+  return (user as Admin).role !== undefined
+}
