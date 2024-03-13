15537bd7f9364e7c196c32ca074b53171ed25271
Alexander Sosov <luisodiz@yandex.ru>
2024-03-13 21:40:34 +0300

    методы класса

---
+++ classes/3.ts
@@ -0,0 +1,38 @@
+// Методы класса
+
+enum PaymentStatus {
+  Holded,
+  Processed,
+  Reversed
+}
+
+class Payment {
+  id: number
+  status: PaymentStatus = PaymentStatus.Holded
+  createdAt: Date = new Date()
+  updatedAt!: Date
+
+  constructor(id: number) {
+    this.id = id
+  }
+
+  getPaymentLifeTime(): number {
+    return new Date().getTime() - this.createdAt.getTime()
+  }
+
+  unholdPayment(): void {
+    if (this.status == PaymentStatus.Processed) {
+      throw new Error('Платёж не может быть возвращен')
+    }
+    this.status = PaymentStatus.Reversed
+    this.updatedAt = new Date()
+  }
+}
+
+const payment = new Payment(1)
+payment.unholdPayment()
+console.log(payment)
+const time = payment.getPaymentLifeTime()
+console.log(time)
+
+export {}
