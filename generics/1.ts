823f028ec420934b99520c23f938c694756c5fa8
Alexander Sosov <luisodiz@yandex.ru>
2024-03-24 17:33:42 +0300

    встроенные generic

---
+++ generics/1.ts
@@ -0,0 +1,16 @@
+// Встроенные generics
+
+const num: Array<number> = [1, 2, 3]
+
+async function test() {
+  const a = await new Promise<number>((resolve, reject) => {
+    resolve(1)
+  })
+}
+
+const check: Record<string, boolean> = {
+  drive: true,
+  kpp: false
+}
+
+export {}
