da7d8f7b5032964da026014548a119e8f1e17abd
Alexander Sosov <luisodiz@yandex.ru>
2024-03-24 17:28:23 +0300

    композиция против наследования

---
+++ classes/8.ts
@@ -0,0 +1,54 @@
+// Композиция против наследования
+
+class User {
+    name: string
+
+    constructor(name: string) {
+        this.name = name
+    }
+}
+
+class Users extends Array<User> {
+    searchByName(name: string) {
+        this.filter(u => u.name === name)
+    }
+
+    override toString(): string {
+        return this.map(u => u.name).join(', ')
+    }
+}
+
+const users = new Users()
+users.push(new User('Вася'))
+users.push(new User('Петя'))
+
+console.log(users.toString())
+
+//
+class UsersList {
+    users!: User[]
+
+    push(u: User) {
+        this.users.push(u)
+    }
+}
+
+class Payment {
+    date?: Date
+}
+
+class UserWithPayment extends Payment {
+    name?: string
+}
+
+class UserWithPayment2 {
+    user: User
+    payment: Payment
+
+    constructor(user: User, payment: Payment) {
+        this.user = user
+        this.payment = payment
+    }
+}
+
+export {}
