fdce498a11999d6a64728b03066ef0db1de5e67d
Alexander Sosov <luisodiz@yandex.ru>
2024-03-24 17:30:49 +0300

    типизация this

---
+++ classes/12.ts
@@ -0,0 +1,31 @@
+// Типизация this
+
+class UserBuilder {
+  name!: string
+
+  setName(name: string): this {
+    this.name = name
+    return this
+  }
+
+  isAdmin(): this is AdminBuilder {
+    return this instanceof AdminBuilder
+  }
+}
+
+class AdminBuilder extends UserBuilder {
+  roles!: string[]
+}
+
+const res = new UserBuilder().setName('Вася')
+const res2 = new AdminBuilder().setName('Вася')
+
+let user: UserBuilder | AdminBuilder = new UserBuilder()
+
+if (user.isAdmin()) {
+  console.log(user)
+} else {
+  console.log(user)
+}
+
+export {}
