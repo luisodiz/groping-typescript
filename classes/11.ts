d1ff74d5a306fb5a2b8affbb0ef7a8f712328eb9
Alexander Sosov <luisodiz@yandex.ru>
2024-03-24 17:30:37 +0300

    работа с this

---
+++ classes/11.ts
@@ -0,0 +1,35 @@
+// Работа с this
+
+class Payment {
+  private date: Date = new Date()
+
+  getDate(this: Payment) {
+    return this.date
+  }
+
+  getDateArrow = () => {
+    return this.date
+  }
+}
+
+const p = new Payment()
+console.log(p.getDate())
+
+const user = {
+  id: 1,
+  paymentDate: p.getDate.bind(p),
+  paymentDateArrow: p.getDateArrow
+}
+//
+// console.log(user.paymentDate());
+// console.log(user.paymentDateArrow())
+
+class PaymentPersistent extends Payment {
+  save() {
+    return this.getDateArrow()
+  }
+}
+
+console.log(new PaymentPersistent().save())
+
+export {}
