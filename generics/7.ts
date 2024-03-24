73850562692f39e7bcd0fbc5b69ac31ed2f6f5f9
Alexander Sosov <luisodiz@yandex.ru>
2024-03-24 17:38:55 +0300

    mixins

---
+++ generics/7.ts
@@ -0,0 +1,43 @@
+// Mixins
+
+type Constructor = new (...args: any[]) => {}
+type GConstructor<T = {}> = new (...args: any[]) => T
+
+class List {
+  constructor(public items: string[]) {
+  }
+}
+
+class Accordion {
+  isOpened!: boolean
+}
+
+type ListType = GConstructor<List>
+type AccordionType = GConstructor<Accordion>
+
+class ExtendedListClass extends List {
+  first() {
+    return this.items[0]
+  }
+}
+
+function ExtendedList<TBase extends ListType & AccordionType>(Base: TBase) {
+  return class ExtendedList extends Base {
+    first() {
+      return this.items[0]
+    }
+  }
+}
+
+class AccordionList {
+  isOpened!: boolean
+
+  constructor(public items: string[]) {
+  }
+}
+
+const list = ExtendedList(AccordionList)
+const res = new list(['first', 'second'])
+
+console.log(res.first())
+export {}
