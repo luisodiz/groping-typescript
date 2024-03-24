6ffc34174f425feb95b28249d33287b57a48f543
Alexander Sosov <luisodiz@yandex.ru>
2024-03-24 17:36:58 +0300

    упражнение: создать метод toString для разных типов

---
+++ generics/example.ts
@@ -0,0 +1,29 @@
+// Написать метод преобразования в строку в зависимости от типа
+
+function toString<T>(data: T): string | undefined {
+  if (Array.isArray(data)) {
+    return data.toString()
+  }
+
+  switch (typeof data) {
+    case 'string':
+      return data
+    case 'number':
+    case 'symbol':
+    case 'bigint':
+    case 'boolean':
+    case 'function':
+      return data.toString()
+    case 'object':
+      return JSON.stringify(data)
+    default:
+      return undefined
+  }
+}
+
+console.log(toString<number>(3))
+console.log(toString(true))
+console.log(toString(['a', 'b']))
+console.log(toString({a: 1}))
+
+export {}
